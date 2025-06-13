1109afc4d1c02bb40d5db65194355d70d24c043f
aditi@05 <aditisinha372@gmail.com>
2025-06-13 11:58:24 +0530

    add these changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           count is {count}
         </button>
         <h1>My first React App.</h1>
+        <h1>Hello I am Aditi Sinha</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
